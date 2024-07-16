59cf375bc6083750e795a079c0d2b60205eca02c
Abhishekk-AK <abhishek520abc@gmail.com>
2024-07-16 20:07:09 +0530

    Login functionalityadded

--- backend/index.js
+++ backend/index.js
@@ -72,6 +72,56 @@ app.post("/register", async(req, res)=>{
     }
 })
 
+app.get("/login", async(req, res)=>{
+    {
+        try {
+            //get all the data from the request body
+            const {email, password} = req.body; 
+
+            //check that all the data should exist
+            if(!(email && password)){
+                return res.status(400).send("Please enter all the details!");
+            }
+
+            //find the user in the database
+            const user = await User.findOne({email});
+            if(!user)
+            {
+                return res.status(401).send("User not found!");
+            }
+
+            //match the password
+            const enteredPassword = await bcrypt.compare(password, user.password);
+            if(!enteredPassword){
+                return res.status(401).send("Password is incorrect.");
+            }
+
+            //generate the token
+            const token = jwt.sign({id: user.id}, process.env.SECRET_KEY, {
+                expiresIn: "1d"
+            });
+            user.token = token;
+            user.password = undefined;
+
+            //store cookies
+            const options = {
+                expires: new Date(Date.now() + 1*24*60*60*1000),
+                httpOnly: true //only manipulate by server not by client/user
+            };
+
+            //send the token
+            res.status(200).cookie("token", token, options).json({
+                message: "You have successfully logged in!",
+                success: true,
+                token
+            });
+        } 
+        catch (error) {
+            console.error(error);
+        }
+    }
+});
+
 
 app.listen(8000, ()=>{
     console.log("Server is listening on port 8000");
